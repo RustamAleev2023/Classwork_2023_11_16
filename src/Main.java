4a31487a55457139f926e9f8910dc22ea8012a22
RustamAleev2023 <ridermix@yandex.ru>
2023-11-16 19:55:05 +0200

    Task2-5 done

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        task1();
-        task2();
+//        task2();
+//        task3();
+//        task4();
+        task5();
     }
 
     //Task1
@@ -36,7 +39,7 @@ public class Main {
             rightSum += right[i];
         }
 
-        if(leftSum == rightSum) {
+        if (leftSum == rightSum) {
             System.out.println("Вы ввели счастливое число");
         } else {
             System.out.println("Вы ввели не счастливое число");
@@ -44,10 +47,92 @@ public class Main {
     }
 
     //Task2
-    public static void task2(){
+    //В введенном шестизначном числе поменять местами 1 и 6, 2 и 5 цифры
+    public static void task2() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите шестизначное число");
 
+        int number = scanner.nextInt();
 
+        if (String.valueOf(number).length() != 6) {
+            System.out.println("Введенное число не является шестизначным");
+        }
+        int[] digits = new int[6];
+        for (int i = 0; i < digits.length; i++) {
+            digits[i] = number % 10;
+            number = number / 10;
+        }
+        for (int i = 0; i < 2; i++) {
+            int temp;
+            temp = digits[i];
+            digits[i] = digits[digits.length - 1 - i];
+            digits[digits.length - 1 - i] = temp;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder(6);
+        for (int i = 0; i < digits.length; i++) {
+            stringBuilder.append(digits[i]);
+        }
+        int result = Integer.parseInt(stringBuilder.reverse().toString());
+
+        System.out.println(result);
+    }
+
+    //Task3
+    //Времена года
+    public static void task3() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер месяца (1 - 12)");
+
+        int num = scanner.nextInt();
+
+        switch (num) {
+            case 1, 2, 12 -> System.out.println("Winter");
+            case 3, 4, 5 -> System.out.println("Spring");
+            case 6, 7, 8 -> System.out.println("Autumn");
+        }
+    }
+
+    //Task4
+    //Месяцы
+    public static void task4() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер месяца (1 - 12)");
+
+        int num = scanner.nextInt();
+
+        switch (num) {
+            case 1 -> System.out.println("January");
+            case 2 -> System.out.println("February");
+            case 3 -> System.out.println("March");
+            case 4 -> System.out.println("April");
+            case 5 -> System.out.println("May");
+            case 6 -> System.out.println("June");
+            case 7 -> System.out.println("July");
+            case 8 -> System.out.println("August");
+            case 9 -> System.out.println("September");
+            case 10 -> System.out.println("October");
+            case 11 -> System.out.println("November");
+            case 12 -> System.out.println("December");
+        }
+    }
+
+    //Task5
+    //Fizz-Buzz
+    public static void task5() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число от 1 до 100");
+
+        int number = scanner.nextInt();
+
+        if (number % 3 == 0 && !(number % 5 == 0)) {
+            System.out.println("Fizz");
+        }
+        if (number % 5 == 0 && !(number % 3 == 0)) {
+            System.out.println("Buzz");
+        }
+        if (number % 15 == 0) {
+            System.out.println("Fizz Buzz");
+        }
     }
 }
